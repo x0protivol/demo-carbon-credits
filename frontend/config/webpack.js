1431548934a9c79bd7ed119af4eb35c7fcc9e366
x0protivol <94541118+x0protivol@users.noreply.github.com>
2023-08-16 20:20:07 +0530

    Update webpack.js

--- frontend/config/webpack.js
+++ frontend/config/webpack.js
@@ -1,4 +1,4 @@
-const zeppelinSolidityHotLoaderOptions = {
+ const zeppelinSolidityHotLoaderOptions = {
   network: 'development',
   // you can stop loader from automatic compile/push/updgrade
   // action by setting disabled flag to true, but it will still
